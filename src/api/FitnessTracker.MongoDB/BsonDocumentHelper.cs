cd96cde38f64c684e8802d54a3241af71c99a423
John Lace <John.Lace@oakbrookfinance.com>
2019-11-25 15:31:08 +0000

    Code tidy up

--- src/api/FitnessTracker.MongoDB/BsonDocumentHelper.cs
+++ src/api/FitnessTracker.MongoDB/BsonDocumentHelper.cs
@@ -7,22 +7,22 @@ namespace FitnessTracker.MongoDB
     {
         public static BsonDocument Map(string name, BsonDocument value)
         {
-            return new BsonDocument { { name, value } };
+            return new BsonDocument {{name, value}};
         }
 
         public static BsonDocument Map(string name, BsonArray values)
         {
-            return new BsonDocument { { name, values } };
+            return new BsonDocument {{name, values}};
         }
 
         public static BsonDocument Map(string name, int value)
         {
-            return new BsonDocument { { name, value } };
+            return new BsonDocument {{name, value}};
         }
 
         public static BsonDocument Map(string name, DateTime value)
         {
-            return new BsonDocument { { name, value } };
+            return new BsonDocument {{name, value}};
         }
     }
 }
